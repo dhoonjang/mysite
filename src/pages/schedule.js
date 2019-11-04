585019fc97e8db2fa51a09e98137ae38d46fb2f2
dhoonjang <dhoonjang@gmail.com>
2019-11-05 06:11:11 +0900

    fix: redirec uri

--- src/pages/schedule.js
+++ src/pages/schedule.js
@@ -3,11 +3,16 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const Schedule = () => {
+  const redirect_uri = window.location.origin
+
+  console.log(window)
   return (
     <Layout>
       <SEO title="Schedule" />
       <h2>Schedule</h2>
-      <a href="https://kauth.kakao.com/oauth/authorize?client_id=839c5882660321b55d631e0eedc8b2e3&redirect_uri=http://localhost:8000/auth&response_type=code">
+      <a
+        href={`https://kauth.kakao.com/oauth/authorize?client_id=839c5882660321b55d631e0eedc8b2e3&redirect_uri=${redirect_uri}/auth&response_type=code`}
+      >
         Login
       </a>
     </Layout>
